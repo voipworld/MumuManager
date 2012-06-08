630e9a81401e540d265d545de2ce11a83bdd8206
voipworld <tayeb.meftah@gmail.com>
2012-06-08 16:18:52 +0300

    Inicial Home page modification

--- web/protected/views/site/index.php
+++ web/protected/views/site/index.php
@@ -1,16 +1,4 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
-	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<h1> MumuDVB Manager</h1>
+<p>MumuManager is a open source project to manage, configure and monitor your MumuDVB instances</p>
+<p>still in developmant!</p>
