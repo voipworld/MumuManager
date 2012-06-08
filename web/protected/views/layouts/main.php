39626e264f9c22d5f14e32cd9b3adf1cabe90362
Tayeb <tayeb.meftah@gmail.com>
2012-06-08 16:34:40 -0400

    Added a Node form model to add a new MumuDVB node

--- web/protected/views/layouts/main.php
+++ web/protected/views/layouts/main.php
@@ -29,7 +29,8 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+array('label'=>'New Node', 'url'=>array('/site/newnode')),
+				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about'))
 //				array('label'=>'Contact', 'url'=>array('/site/contact'))
 //				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 //				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
@@ -47,7 +48,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by Tayeb Meftah.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
