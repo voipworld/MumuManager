3393e3016a29af8388f37e2f2426ccac30e4f089
voipworld <tayeb.meftah@gmail.com>
2012-06-09 19:09:37 +0300

    Removing YII Framework and switching to  CodeEgniter a suggested by Mr Youcef Haouatis thank you.

--- README.md
+++
@@ -1,4 +0,0 @@
-MumuManager
-===========
-
-a MumuDVB frontend for quick setup and management of a IPTV Streaming platform
--- web/protected/views/site/index.php
+++
@@ -1,4 +0,0 @@
-<?php $this->pageTitle=Yii::app()->name; ?>
-<h1> MumuDVB Manager</h1>
-<p>MumuManager is a open source project to manage, configure and monitor your MumuDVB instances</p>
-<p>still in developmant!</p>
